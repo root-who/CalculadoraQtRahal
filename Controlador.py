6973d5952b6573abb8c54906942ef03404c974d8
root_who <joaopaulo_gonzaga@outlook.com>
2022-06-06 00:09:14 -0300

    resultado agregado

--- Controlador.py
+++ Controlador.py
@@ -14,8 +14,9 @@ from Model.CalculadoraSimplesModel import CalculadoraSimplesModel
 
 class controller:
     def __init__(self):
-        self.num = "0";
-        self.result = "";
+        self.num1 = "0";
+        self.num2 = "0";
+        self.result = "0";
         self.operacao = "";
         self.operadorClicked = False;
         self.TelaInicial_Window = QtWidgets.QMainWindow()
@@ -34,23 +35,23 @@ class controller:
 
         # Define os botões da Calculadora Simples
 
-        self.tradicional_ui.button_0.clicked.connect(lambda: self.setNumBussiness("0"))    # 0
-        self.tradicional_ui.button_1.clicked.connect(lambda: self.setNumBussiness("1"))    # 1
-        self.tradicional_ui.button_2.clicked.connect(lambda: self.setNumBussiness("2"))     # 2
-        self.tradicional_ui.button_3.clicked.connect(lambda: self.setNumBussiness("3"))    # 3
-        self.tradicional_ui.button_4.clicked.connect(lambda: self.setNumBussiness("4"))    # 4
-        self.tradicional_ui.button_5.clicked.connect(lambda: self.setNumBussiness("5"))    # 5
-        self.tradicional_ui.button_6.clicked.connect(lambda: self.setNumBussiness("6"))    # 6
-        self.tradicional_ui.button_7.clicked.connect(lambda: self.setNumBussiness("7"))     # 7
-        self.tradicional_ui.button_8.clicked.connect(lambda: self.setNumBussiness("8"))     # 8
-        self.tradicional_ui.button_9.clicked.connect(lambda: self.setNumBussiness("9"))     # 9
+        self.tradicional_ui.button_0.clicked.connect(lambda: self.isOperatorClicked("0"))    # 0
+        self.tradicional_ui.button_1.clicked.connect(lambda: self.isOperatorClicked("1"))    # 1
+        self.tradicional_ui.button_2.clicked.connect(lambda: self.isOperatorClicked("2"))     # 2
+        self.tradicional_ui.button_3.clicked.connect(lambda: self.isOperatorClicked("3"))    # 3
+        self.tradicional_ui.button_4.clicked.connect(lambda: self.isOperatorClicked("4"))    # 4
+        self.tradicional_ui.button_5.clicked.connect(lambda: self.isOperatorClicked("5"))    # 5
+        self.tradicional_ui.button_6.clicked.connect(lambda: self.isOperatorClicked("6"))    # 6
+        self.tradicional_ui.button_7.clicked.connect(lambda: self.isOperatorClicked("7"))     # 7
+        self.tradicional_ui.button_8.clicked.connect(lambda: self.isOperatorClicked("8"))     # 8
+        self.tradicional_ui.button_9.clicked.connect(lambda: self.isOperatorClicked("9"))     # 9
         self.tradicional_ui.button_add.clicked.connect(lambda: self.setOperacao("soma"))  # +
         self.tradicional_ui.button_subtraction.clicked.connect(lambda: self.setOperacao("subtr"))  # -
         self.tradicional_ui.button_multiple.clicked.connect(lambda: self.setOperacao("multi"))   # x
         self.tradicional_ui.button_division.clicked.connect(lambda: self.setOperacao("divi"))   # ÷
-        self.tradicional_ui.button_sqrt.clicked.connect(lambda: self.setOperacao("raiz"))   # √
-        self.tradicional_ui.button_comann.clicked.connect(lambda: self.setNumBussiness(","))   # ,
-        self.tradicional_ui.button_equal.clicked.connect(self.igual)     # =
+        self.tradicional_ui.button_sqrt.clicked.connect(lambda: self.raiz())   # √
+        self.tradicional_ui.button_comann.clicked.connect(lambda: self.isOperatorClicked(","))   # ,
+        self.tradicional_ui.button_equal.clicked.connect(lambda: self.igual())     # =
         self.tradicional_ui.button_delete_all.clicked.connect(self.ac)           # AC
         self.tradicional_ui.button_delete.clicked.connect(self.delete)     # DEL
 
@@ -81,33 +82,65 @@ class controller:
         # self.cientifica_ui.pushButton_36.clicked.connect(self.ac)         # AC
         # self.cientifica_ui.pushButton_31.clicked.connect(self.delete)     # DEL
     
-    def setNum(self, value, iszero):
+    def setNum2(self, value, iszero):
         if iszero:
-            self.num = value    
-            self.tradicional_ui.label_display.setText(self.num)
+            self.num2 = value    
+            self.tradicional_ui.label_display.setText(self.num2)
         else:
-            self.num = self.num + value;
-            self.tradicional_ui.label_display.setText(self.num)
+            self.num2 = self.num2 + value;
+            self.tradicional_ui.label_display.setText(self.num2)
+            
+    def setNum1(self, value, iszero):
+        if iszero:
+            self.num1 = value    
+            self.tradicional_ui.label_display.setText(self.num1)
+        else:
+            self.num1 = self.num1 + value;
+            self.tradicional_ui.label_display.setText(self.num1)
+ 
+    def isOperatorClicked(self, value):
+        print("aqui " + str(self.operadorClicked))
+        if self.operadorClicked:
+            self.setNumBussiness(self.num2, value, True)
+        else: self.setNumBussiness(self.num1, value, False)
     
-    def setNumBussiness(self, value):
-        if not self.operadorClicked:
-            self.result=""
-        if not self.num.find(",") == -1 and value == ",":
-            pass 
-        elif self.num == "0" and value == ",":
-            self.setNum(value, False)
-        elif self.num == "0": 
-            self.setNum(value, True)
+        print(self.num1 + " " + self.num2)
+            
+    def setNumBussiness(self, var, value, funcao):
+        if not var.find(",") == -1 and value == ",":
+                pass 
+        elif var == "0" and value == ",":
+            if funcao : self.setNum2(value, False) 
+            else: self.setNum1(value, False)
+        elif var == "0": 
+             if funcao : self.setNum2(value, True)
+             else: self.setNum1(value, True)
         else:    
-            self.setNum(value, False)
-            print(self.num)
-    
+            if funcao : self.setNum2( value, False)
+            else: self.setNum1( value, False)
+
+
+    def raiz(self):
+        if self.result == "0":
+            self.result = self.calculator.raiz(self.num1)
+            self.tradicional_ui.label_display.setText(self.result)
+        else:
+            self.result = self.calculator.raiz(self.result)
+            self.tradicional_ui.label_display.setText(self.result)
+        
+        
     def setOperacao(self, value):
-        self.result = self.num
-        self.num="0"
-        self.tradicional_ui.label_display.setText(self.num)
-        self.operacao = value
-        self.operadorClicked = True 
+        
+        if self.operadorClicked == True and self.result != "0":
+            self.igualOperacaoAgregada(self.result, self.num2)
+
+            self.operacao = value
+        elif self.operadorClicked == True and self.result == "0":
+            self.igualOperacaoAgregada(self.num1, self.num2)
+            self.operacao = value
+        else:
+            self.operacao = value
+            self.operadorClicked = True 
     
     
     def delete(self):
@@ -117,12 +150,28 @@ class controller:
     def ac(self):
         pass
     
+    def igualOperacaoAgregada(self, num1, num2):
+        self.result = self.calculator.operacao(num1, num2, self.operacao)
+        self.tradicional_ui.label_display.setText(self.result)
+        print("NUM1 "+num1)
+        print("NUM2 "+num2)
+        print("RESULTADO " + self.result)
+        self.num1="0"
+        self.num2="0"
+    
     def igual(self):
         self.operadorClicked = False
-        self.result = self.calculator.operacao(self.result, self.num, self.operacao)
-        self.tradicional_ui.label_display.setText(self.result)
-        self.num=""
-        print(self.num + " " + self.result)
+        if self.result == "0":
+            self.result = self.calculator.operacao(self.num1, self.num2, self.operacao)
+            self.tradicional_ui.label_display.setText(self.result)
+        else: 
+            self.result = self.calculator.operacao(self.result, self.num2, self.operacao)
+            self.tradicional_ui.label_display.setText(self.result)
+        print("NUM1 "+self.num1)
+        print("NUM2 "+self.num2)
+        print("RESULTADO " + self.result)
+        self.num1="0"
+        self.num2="0"
 
     def show_TelaInicial(self):
         self.TelaInicial_Window.show()
